91ec62f47799fc01885be7d89ad956c14430e2c9
Pupino <romina.jalon@gmail.com>
2022-06-13 22:27:31 -0300

    Adding maps and promises

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
+
+const Item = ({ item }) => {
+
+    const divBadgeStyles = {
+        top: '0.5rem',
+        right: '0.5rem'
+    }
+
+  return (
+    <div class="col mb-5">
+        <div class="card h-100">
+            {/* <!-- Sale badge--> */}
+            <div class="badge bg-dark text-white position-absolute" style={divBadgeStyles}>Sale</div>
+            {/* <!-- Product image--> */}
+            <img class="card-img-top" src={item.img} alt="..." />
+            {/* <!-- Product details--> */}
+            <div class="card-body p-4">
+                <div class="text-center">
+                    {/* <!-- Product name--> */}
+                    <h5 class="fw-bolder">{item.name}</h5>
+                    {/* <!-- Product price--> */}
+                    ${item.price}
+                </div>
+            </div>
+            {/* <!-- Product actions--> */}
+            <ItemCount stock={5} initial={1}/>
+        </div>
+    </div>
+  )
+}
+
+export default Item
---
+++ src/data/products.js
@@ -0,0 +1,11 @@
+export const productsData = [
+    {"id":1,"name":"Colobus guerza","img":"./img/tejido1.png","price":48},
+{"id":2,"name":"Butorides striatus","img":"./img/tejido2.png","price":13},
+{"id":3,"name":"Phalacrocorax carbo","img":"./img/tejido3.png","price":87},
+{"id":4,"name":"Neophron percnopterus","img":"./img/tejido1.png","price":46},
+{"id":5,"name":"Boselaphus tragocamelus","img":"./img/tejido1.png","price":18},
+{"id":6,"name":"Conolophus subcristatus","img":"./img/tejido2.png","price":100},
+{"id":7,"name":"Taurotagus oryx","img":"./img/tejido3.png","price":41},
+{"id":8,"name":"Tachybaptus ruficollis","img":"./img/tejido1.png","price":71},
+{"id":9,"name":"Paradoxurus hermaphroditus","img":"./img/tejido1.png","price":18},
+{"id":10,"name":"Cochlearius cochlearius","img":"./img/tejido2.png","price":88}];
